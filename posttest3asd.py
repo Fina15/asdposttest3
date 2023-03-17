9b90c02f495edf2f886dbb0ac9a0ff0768a064a8
Fina <127528115+Fina15@users.noreply.github.com>
2023-03-17 21:00:01 +0800

    Update posttest3asd.py

--- posttest3asd.py
+++ posttest3asd.py
@@ -2,40 +2,90 @@ import os
 os.system('cls')
 from prettytable import PrettyTable
 
+# Membuat kelas Node untuk Linked List
 class Node:
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 
+# Membuat kelas LinkedList untuk toko sepatu
 class LinkedList:
     def __init__(self):
         self.head = None
 
+    # Menambahkan sepatu ke Linked List
     def add(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        curr_node = self.head
-        while curr_node.next is not None:
-            curr_node = curr_node.next
-        curr_node.next = new_node
+        node = Node(data)
+        if not self.head:
+            self.head = node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = node
 
+    # Menghapus sepatu dari Linked List berdasarkan nomor urut pada tabel
+    def delete(self, num):
+        if not self.head:
+            print("List is empty")
+        elif num == 1:
+            self.head = self.head.next
+        else:
+            current = self.head
+            prev = None
+            count = 1
+            while current and count != num:
+                prev = current
+                current = current.next
+                count += 1
+            if count == num:
+                prev.next = current.next
+            else:
+                print("Invalid node number")
+
+    # Menampilkan daftar sepatu dengan menggunakan prettytable
     def display(self):
-        if self.head is None:
-            print("LinkedList is empty")
-            return
-        x = PrettyTable()
-        x.field_names = ["ID", "Brand", "Type", "Color", "Size", "Price"]
-        curr_node = self.head
-        while curr_node is not None:
-            x.add_row(curr_node.data)
-            curr_node = curr_node.next
-        print(x)
+        table = PrettyTable()
+        table.field_names = ["No.", "Merk", "Harga"]
+        current = self.head
+        count = 1
+        while current:
+            table.add_row([count, current.data["merk"], current.data["harga"]])
+            current = current.next
+            count += 1
+        print(table)
 
-# Contoh penggunaan
-ll = LinkedList()
-ll.add([1, "Nike", "Running Shoes", "Black", "9", 1500000])
-ll.add([2, "Adidas", "Sneakers", "White", "8", 1200000])
-ll.add([3, "Puma", "Football Shoes", "Red", "10", 2000000])
-ll.display()
+# Inisialisasi Linked List dengan beberapa data sepatu
+shoe_list = LinkedList()
+shoe_list.add({"merk": "Nike", "harga": 500000})
+shoe_list.add({"merk": "Adidas", "harga": 700000})
+shoe_list.add({"merk": "Puma", "harga": 600000})
+
+# Menu utama
+while True:
+    print("\nMenu:")
+    print("1. Tambah sepatu")
+    print("2. Hapus sepatu")
+    print("3. Tampilkan daftar sepatu")
+    print("4. Keluar")
+    choice = input("Pilih menu (1/2/3/4): ")
+
+    if choice == "1":
+        merk = input("Masukkan merk sepatu: ")
+        harga = int(input("Masukkan harga sepatu: "))
+        shoe_list.add({"merk": merk, "harga": harga})
+        print("Sepatu berhasil ditambahkan")
+
+    elif choice == "2":
+        num = int(input("Masukkan nomor urut sepatu yang ingin dihapus: "))
+        shoe_list.delete(num)
+        print("Sepatu berhasil dihapus")
+
+    elif choice == "3":
+        shoe_list.display()
+
+    elif choice == "4":
+        break
+
+    else:
+        print("Menu tidak tersedia")
