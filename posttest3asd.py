544d36139a082f22d1485d204cd4a0460bade04e
Fina <127528115+Fina15@users.noreply.github.com>
2023-03-17 11:26:55 +0800

    Update posttest3asd.py

--- posttest3asd.py
+++ posttest3asd.py
@@ -1,39 +1,41 @@
-import os 
+import os
 os.system('cls')
+from prettytable import PrettyTable
 
 class Node:
-    def __init__(self, merk, harga):
-        self.merk = merk
-        self.harga = harga
-        self.next = None
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
 
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def add_node(self, merk, harga):
-        new_node = Node(merk, harga)
+    def add(self, data):
+        new_node = Node(data)
         if self.head is None:
             self.head = new_node
-        else:
-            current = self.head
-            while current.next is not None:
-                current = current.next
-            current.next = new_node
+            return
+        curr_node = self.head
+        while curr_node.next is not None:
+            curr_node = curr_node.next
+        curr_node.next = new_node
 
-    def display_list(self):
+    def display(self):
         if self.head is None:
-            print("List kosong")
-        else:
-            current = self.head
-            while current is not None:
-                print("Merk: ", current.merk, ">>>", "Harga: ", current.harga)
-                current = current.next
+            print("LinkedList is empty")
+            return
+        x = PrettyTable()
+        x.field_names = ["ID", "Brand", "Type", "Color", "Size", "Price"]
+        curr_node = self.head
+        while curr_node is not None:
+            x.add_row(curr_node.data)
+            curr_node = curr_node.next
+        print(x)
 
-# contoh penggunaan linked list untuk toko sepatu
-sepatu_list = LinkedList()
-sepatu_list.add_node("Adidas", 800000)
-sepatu_list.add_node("Nike", 700000)
-sepatu_list.add_node("Puma", 600000)
-
-sepatu_list.display_list()
+# Contoh penggunaan
+ll = LinkedList()
+ll.add([1, "Nike", "Running Shoes", "Black", "9", 1500000])
+ll.add([2, "Adidas", "Sneakers", "White", "8", 1200000])
+ll.add([3, "Puma", "Football Shoes", "Red", "10", 2000000])
+ll.display()
